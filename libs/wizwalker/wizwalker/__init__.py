ca023bb0c332ac7b6730a97725722cf2dd8a6a43
Cayden Tisdale <caydentisdale43@gmail.com>
2026-07-30 09:12:32 -0400

    fix(wizwalker): export discovered client on Windows

--- libs/wizwalker/wizwalker/__init__.py
+++ libs/wizwalker/wizwalker/__init__.py
@@ -23,9 +23,9 @@ if sys.platform == "win32":
 else:
     from . import memory
     from .client_handler import ClientHandler
-    from .discovered_client import DiscoveredClient
     from .memory import DeimosNativeMemoryBackend, MemoryBackend, MemoryReader
 
+from .discovered_client import DiscoveredClient
 from .telemetry import (
     ReadOnlyTelemetryReader,
     ReadOnlyTelemetrySnapshot,
