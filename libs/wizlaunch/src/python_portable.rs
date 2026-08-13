847421c21043ef397eff5944b38e8747afd0c372
Cayden Tisdale <caydentisdale43@gmail.com>
2026-08-13 14:54:18 -0400

    fix(ci): restore cross-platform package builds

--- libs/wizlaunch/src/python_portable.rs
+++ libs/wizlaunch/src/python_portable.rs
@@ -2,7 +2,7 @@ use pyo3::prelude::*;
 
 #[pymodule]
 pub fn _native(module: &Bound<'_, PyModule>) -> PyResult<()> {
-    module.add("__version__", "0.2.0")?;
+    module.add("__version__", "0.3.1")?;
     module.add("WINDOWS_ACCOUNT_BACKEND", false)?;
     Ok(())
 }
